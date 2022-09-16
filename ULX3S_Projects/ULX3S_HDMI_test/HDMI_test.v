f44421931179d96d5b3651abea0dce9e6b1e898d
FeldmeierMichael <mfeldmei@hm.edu>
2022-09-16 14:15:10 +0200

    ULX3S_HDMI_test working

--- ULX3S_Projects/ULX3S_HDMI_test/HDMI_test.v
+++ ULX3S_Projects/ULX3S_HDMI_test/HDMI_test.v
@@ -3,8 +3,8 @@
 ////////////////////////////////////////////////////////////////////////
 module HDMI_test(
 	input pixclk,  // 25MHz
-	output [2:0] TMDSp, TMDSn,
-	output TMDSp_clock, TMDSn_clock
+	output [3:0] TMDSd
+	
 	
 );
 
@@ -35,7 +35,8 @@ TMDS_encoder encode_B(.clk(pixclk), .VD(blue ), .CD({vSync,hSync}), .VDE(DrawAre
 
 ////////////////////////////////////////////////////////////////////////
 wire clk_TMDS, DCM_TMDS_CLKFX;  // 25MHz x 10 = 250MHz
-DCM_SP #(.CLKFX_MULTIPLY(10)) DCM_TMDS_inst(.CLKIN(pixclk), .CLKFX(clk_TMDS), .RST(1'b0));
+clock clockmty(.clkin_25MHz(pixclk),.clk_250MHz(clk_TMDS));
+//DCM_SP #(.CLKFX_MULTIPLY(10)) DCM_TMDS_inst(.CLKIN(pixclk), .CLKFX(clk_TMDS), .RST(1'b0));
 //BUFG BUFG_TMDSp(.I(DCM_TMDS_CLKFX), .O(clk_TMDS));
 
 ////////////////////////////////////////////////////////////////////////
@@ -52,10 +53,10 @@ begin
 	TMDS_mod10 <= (TMDS_mod10==4'd9) ? 4'd0 : TMDS_mod10+4'd1;
 end
 
-OBUFDS OBUFDS_red  (.I(TMDS_shift_red  [0]), .O(TMDSp[2]), .OB(TMDSn[2]));
-OBUFDS OBUFDS_green(.I(TMDS_shift_green[0]), .O(TMDSp[1]), .OB(TMDSn[1]));
-OBUFDS OBUFDS_blue (.I(TMDS_shift_blue [0]), .O(TMDSp[0]), .OB(TMDSn[0]));
-OBUFDS OBUFDS_clock(.I(pixclk), .O(TMDSp_clock), .OB(TMDSn_clock));
+OBUFDS OBUFDS_red  (.I(TMDS_shift_red  [0]), .O(TMDSd[2]));
+OBUFDS OBUFDS_green(.I(TMDS_shift_green[0]), .O(TMDSd[1]));
+OBUFDS OBUFDS_blue (.I(TMDS_shift_blue [0]), .O(TMDSd[0]));
+OBUFDS OBUFDS_clock(.I(pixclk), .O(TMDSd[3]));
 endmodule
 
 
@@ -96,6 +97,71 @@ assign OB = ~I;
 
 endmodule
 
+module clock
+		(
+		input clkin_25MHz,
+		output clk_125MHz,
+		output clk_250MHz,
+		output clk_25MHz,
+		output clk_83M333Hz,
+		output locked
+		);
+	wire int_locked;
+
+	(* ICP_CURRENT="9" *) (* LPF_RESISTOR="8" *) (* MFG_ENABLE_FILTEROPAMP="1" *) (* MFG_GMCREF_SEL="2" *)
+		EHXPLLL
+		#(
+			.PLLRST_ENA("DISABLED"),
+			.INTFB_WAKE("DISABLED"),
+			.STDBY_ENABLE("DISABLED"),
+			.DPHASE_SOURCE("DISABLED"),
+			.CLKOS_FPHASE(0),
+			.CLKOP_FPHASE(0),
+			.CLKOS3_CPHASE(5),
+			.CLKOS2_CPHASE(0),
+			.CLKOS_CPHASE(1),
+			.CLKOP_CPHASE(3),
+			.OUTDIVIDER_MUXD("DIVD"),
+			.OUTDIVIDER_MUXC("DIVC"),
+			.OUTDIVIDER_MUXB("DIVB"),
+			.OUTDIVIDER_MUXA("DIVA"),
+			.CLKOS3_ENABLE("ENABLED"),
+			.CLKOS2_ENABLE("ENABLED"),
+			.CLKOS_ENABLE("ENABLED"),
+			.CLKOP_ENABLE("ENABLED"),
+			.CLKOS3_DIV(0),
+			.CLKOS2_DIV(20),
+			.CLKOS_DIV(2),
+			.CLKOP_DIV(4),
+			.CLKFB_DIV(5),
+			.CLKI_DIV(1),
+			.FEEDBK_PATH("CLKOP")
+		)
+		pll_i
+		(
+			.CLKI(clkin_25MHz),
+			.CLKFB(clk_125MHz),
+			.CLKOP(clk_125MHz),
+			.CLKOS(clk_250MHz),
+			.CLKOS2(clk_25MHz), 
+			.CLKOS3(clk_83M333Hz),
+			.RST(1'b0),
+			.STDBY(1'b0),
+			.PHASESEL0(1'b0),
+			.PHASESEL1(1'b0),
+			.PHASEDIR(1'b0),
+			.PHASESTEP(1'b0),
+			.PLLWAKESYNC(1'b0),
+			.ENCLKOP(1'b0),
+			.ENCLKOS(1'b0),
+			.ENCLKOS2(1'b0),
+			.ENCLKOS3(1'b0),
+			.LOCK(locked),
+			.INTLOCK(int_locked)
+		);
+endmodule
 
 
-////////////////////////////////////////////////////////////
+
+
+////////////////////
