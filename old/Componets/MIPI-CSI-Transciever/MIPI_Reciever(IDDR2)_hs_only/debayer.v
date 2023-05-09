6301b9afd4285b8348c4ec4e98319dff34cd0ed9
Michi <mfeldmei@hm.edu>
2023-05-09 20:16:56 +0200

    debayer

---
+++ old/Componets/MIPI-CSI-Transciever/MIPI_Reciever(IDDR2)_hs_only/debayer.v
@@ -0,0 +1,16 @@
+module moduleName (
+        input clock,reset,
+        input[18:0] address_in,
+        output[18:0]address_out,
+        input[7:0] raw,
+        output[7:0] red,
+        output[7:0] green,
+        output[7:0] blue
+    );
+    reg [7:0] line0[639:0];
+    reg [7:0] line1[639:0];
+    reg [7:0] line2[639:0];
+    
+
+    
+endmodule
